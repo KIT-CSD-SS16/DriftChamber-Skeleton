642c9feeae0f76c98f07d1566b59fab8a691450e
Thomas <Thomas.Hauth@web.de>
2015-11-19 14:45:50 +0100

    evaluating return code

--- drift_tests.py
+++ drift_tests.py
@@ -1,11 +1,15 @@
 #! /usr/bin/env python3.4
 
 import unittest
+import sys
 
 
 def main():
     all_test = unittest.defaultTestLoader.discover("")
-    unittest.TextTestRunner(verbosity=2).run(all_test)
+    if unittest.TextTestRunner(verbosity=2).run(all_test).wasSuccessful():
+        sys.exit(0)
+    else:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
